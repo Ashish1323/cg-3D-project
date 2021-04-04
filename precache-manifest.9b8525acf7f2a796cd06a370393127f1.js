d2d8905a6f08e9a87f76704abc231aa69a71fd94
Ashish1323 <ashishagnihotri2k@gmail.com>
2021-04-04 16:52:09 +0530

    Updates

---
+++ precache-manifest.9b8525acf7f2a796cd06a370393127f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ddb2787a0358e99fe23cc1022fc7ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f6e9c1bbf1bc8d45d08",
+    "url": "/static/css/main.50b1b496.chunk.css"
+  },
+  {
+    "revision": "60d81298ebf6109d0d3a",
+    "url": "/static/js/2.c469308d.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.c469308d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6e9c1bbf1bc8d45d08",
+    "url": "/static/js/main.f6196099.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
